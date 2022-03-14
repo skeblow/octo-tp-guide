df9c3ca3859aba143f54be5c2f25e17e3dc12b60
skeblow <skeblow@gmail.com>
2022-03-14 13:17:36 +0100

    init front

---
+++ front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <h1>👋Hello world🌎</h1>
+
+    <img class="logo" src="../assets/dickbutt.png" alt="logo" />
+
+    <p>{{ aaa }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      aaa: 'aaaa',
+    }
+  }
+}
+</script>
