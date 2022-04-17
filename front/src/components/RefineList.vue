271185ab45540c0ec10180d4664bca7309b3ddb8
David Strba <david.strba@maternia.cz>
2022-04-17 12:37:57 +0200

    add search to refine list

--- front/src/components/RefineList.vue
+++ front/src/components/RefineList.vue
@@ -1,50 +1,65 @@
 <template>
-    <table class="table table-stripped">
-        <thead>
-            <tr>
-                <th>Input</th>
-                <th>Output</th>
-                <th>Input buy</th>
-                <th>Input sell</th>
-                <th>Output sell</th>
-                <th>Diff</th>
-                <th>Roi if bought</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="trade in items" :key="trade.recipe.id">
-                <td>
-                    {{ trade.input[0].quantity }}x
-                    <a v-bind:href="'https://www.gw2bltc.com/en/item/'+trade.input[0].item.id" target="_blank">
-                        <img v-bind:src="trade.input[0].item.icon" alt="">
-                    </a>
-                    {{ trade.input[0].item.name }}
-                </td>
-                <td>
-                    {{ trade.output.quantity }}x
-                    <a v-bind:href="'https://www.gw2bltc.com/en/item/'+trade.output.item.id" target="_blank">
-                        <img v-bind:src="trade.output.item.icon" alt="">
-                    </a>
-                    {{ trade.output.item.name }}
-                </td>
-                <td>
-                    {{ formatGold(trade.totalBuy) }}
-                </td>
-                <td>
-                    {{ formatGold(getInputSell(trade)) }}
-                </td>
-                <td>
-                    {{ formatGold(trade.totalSell) }}
-                </td>
-                <td>
-                    {{ formatGold(trade.totalSell - getInputSell(trade)) }}
-                </td>
-                <td>
-                    {{ trade.roi }}%
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <div class="row">
+            <div class="col-6">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search..." v-model="search">
+                    <button 
+                        class="btn btn-outline-secondary" 
+                        type="button"
+                        @click="clearSearch"
+                    >X</button>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-stripped">
+            <thead>
+                <tr>
+                    <th>Input</th>
+                    <th>Output</th>
+                    <th>Input buy</th>
+                    <th>Input sell</th>
+                    <th>Output sell</th>
+                    <th>Diff</th>
+                    <th>Roi if bought</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="trade in getItems()" :key="trade.recipe.id">
+                    <td>
+                        {{ trade.input[0].quantity }}x
+                        <a v-bind:href="'https://www.gw2bltc.com/en/item/'+trade.input[0].item.id" target="_blank">
+                            <img v-bind:src="trade.input[0].item.icon" alt="">
+                        </a>
+                        {{ trade.input[0].item.name }}
+                    </td>
+                    <td>
+                        {{ trade.output.quantity }}x
+                        <a v-bind:href="'https://www.gw2bltc.com/en/item/'+trade.output.item.id" target="_blank">
+                            <img v-bind:src="trade.output.item.icon" alt="">
+                        </a>
+                        {{ trade.output.item.name }}
+                    </td>
+                    <td>
+                        {{ formatGold(trade.totalBuy) }}
+                    </td>
+                    <td>
+                        {{ formatGold(getInputSell(trade)) }}
+                    </td>
+                    <td>
+                        {{ formatGold(trade.totalSell) }}
+                    </td>
+                    <td>
+                        {{ formatGold(trade.totalSell - getInputSell(trade)) }}
+                    </td>
+                    <td>
+                        {{ trade.roi }}%
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script lang="ts">
 import { Options, Vue, prop } from 'vue-class-component';
@@ -57,10 +72,25 @@ class RefineListProps {
 
 @Options({})
 export default class RefineList extends Vue.with(RefineListProps) {
+    search: string = '';
     formatGold = PriceService.formatGold;
 
     getInputSell(trade: RecipeTrade): number {
         return trade.input.reduce((total, input) => total + (input.price.sells.unit_price * input.quantity), 0);
+    }
+
+    clearSearch(): void {
+        this.search = '';
+    }
+
+    getItems(): Array<RecipeTrade> {
+        return this.items
+            .filter(item => item.input.map(input => input.item.name)
+                    .concat([item.output.item.name])
+                    .join(' ')
+                    .toLowerCase()
+                    .includes(this.search),
+            );
     }
 }
 </script>
