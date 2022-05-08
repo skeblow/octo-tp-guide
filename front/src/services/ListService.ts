38c221a69e92acdaef27a135dc4aa1012b30de40
David Strba <david.strba@maternia.cz>
2022-05-08 13:05:01 +0200

    use baseurl in listservice

--- front/src/services/ListService.ts
+++ front/src/services/ListService.ts
@@ -1,64 +1,63 @@
+import { BASE_URL } from "@/config";
 import { BasicTrade, RecipeTrade, SalvageTrade } from "../../../shared";
 
 class ListService {
-    readonly BASE_URL = 'http://localhost:3000';
-
-    async getCheapBasicList(): Promise<Array<BasicTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/cheap');
+    public async getCheapBasicList(): Promise<Array<BasicTrade>> {
+        const res = await fetch(BASE_URL + '/lists/cheap');
 
         return await res.json();
     }
 
     async getExpensiveBasicList(): Promise<Array<BasicTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/expensive');
+        const res = await fetch(BASE_URL + '/lists/expensive');
 
         return await res.json();
     }
 
     async getRefineList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/refine');
+        const res = await fetch(BASE_URL + '/lists/refine');
 
         return await res.json();
     }
 
-    async getSalvageList(): Promise<Array<SalvageTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/salvage');
+    public async getSalvageList(): Promise<Array<SalvageTrade>> {
+        const res = await fetch(BASE_URL + '/lists/salvage');
 
         return await res.json();
     }
 
-    async getCookingList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/cooking');
+    public async getCookingList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/cooking');
 
         return await res.json();
     }
 
-    async getUtilityList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/utility');
+    public async getUtilityList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/utility');
 
         return await res.json();
     }
 
-    async getOpenList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/open');
+    public async getOpenList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/open');
 
         return await res.json();
     }
 
-    async getWeaponsmithList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/weaponsmith');
+    public async getWeaponsmithList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/weaponsmith');
 
         return await res.json();
     }
 
-    async getJewelcraftingList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/jewelcrafting');
+    public async getJewelcraftingList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/jewelcrafting');
 
         return await res.json();
     }
 
-    async getArtificierList(): Promise<Array<RecipeTrade>> {
-        const res = await fetch(this.BASE_URL + '/lists/artificier');
+    public async getArtificierList(): Promise<Array<RecipeTrade>> {
+        const res = await fetch(BASE_URL + '/lists/artificier');
 
         return await res.json();
     }
