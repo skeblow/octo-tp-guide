12c7d7f50ef6fc810ca28ba9a04349349e1f779b
skeblow <skeblow@gmail.com>
2022-05-13 19:54:36 +0200

    add ruby jwl recipes

--- back/src/services/JewelcraftingService.ts
+++ back/src/services/JewelcraftingService.ts
@@ -18,6 +18,21 @@ export default class JewelcraftingService {
                 output: 71686,
                 orichalcumOre: 20 + 4 + 8,
             }),
+            // 13459-Ruby-Orichalcum-Earring
+            this.createRubyOrichalcumRecipe({
+                output: 13459,
+                orichalcumOre: 14 * 2,
+            }),
+            // 13417-Ruby-Orichalcum-Ring
+            this.createRubyOrichalcumRecipe({
+                output: 13417,
+                orichalcumOre: 15 * 2,
+            }),
+            // 13501-Ruby-Orichalcum-Amulet
+            this.createRubyOrichalcumRecipe({
+                output: 13501,
+                orichalcumOre: 16 * 2,
+            }),
         ];
     }
 
@@ -38,6 +53,29 @@ export default class JewelcraftingService {
                 { id: 19721, quantity: 5 },
                 // orichalcum ore
                 { id: 19701, quantity: 32 },
+            ],
+        };
+    }
+
+    private createRubyOrichalcumRecipe(options: {
+        output: number,
+        orichalcumOre: number,
+    }) {
+        return {
+            id: options.output,
+            type: RecipeType.jewelcrafting,
+            output: [
+                { id: options.output, quantity: 1 },
+            ],
+            input: [
+                // orichalcum ore
+                { id: 19701, quantity: options.orichalcumOre },
+                // 19721-Glob-of-Ectoplasm
+                { id: 19721, quantity: 5 },
+                // 24474-Ruby-Crystal
+                { id: 24474, quantity: 10 },
+                // 24276-Pile-of-Incandescent-Dust
+                { id: 24276, quantity: 25 },
             ],
         };
     }
